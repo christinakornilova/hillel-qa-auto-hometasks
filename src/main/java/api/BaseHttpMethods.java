c11c94db1c1c79a6b7b5a50975219f5d319cc6c1
christinakornilova <30318622+fsage@users.noreply.github.com>
2018-07-20 19:51:21 +0300

    Api tests update according to Rob's review.

--- src/main/java/api/BaseHttpMethods.java
+++ src/main/java/api/BaseHttpMethods.java
@@ -8,36 +8,30 @@ import utils.ApiConstants;
 
 public class BaseHttpMethods {
 
-    public static Response sendGet(String path) throws JSONException {
+    private static RequestSpecification composeRequest() {
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
-        return request.get(ApiConstants.HOST + path);
+        return request;
+    }
+
+    public static Response sendGet(String path) throws JSONException {
+        return composeRequest().get(ApiConstants.HOST + path);
     }
 
     public static Response sendPut(String path, int id, String jsonBody) {
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        request.body(jsonBody);
-        return request.put(ApiConstants.HOST + path + "/" + id);
+        return composeRequest().body(jsonBody).put(ApiConstants.HOST + path + "/" + id);
     }
 
     public static Response sendPost(String path, String jsonBody) {
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        request.body(jsonBody);
-        return request.post(ApiConstants.HOST + path);
+        return composeRequest().body(jsonBody).post(ApiConstants.HOST + path);
     }
 
     public static Response sendDelete(String path, int id) {
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-        return request.delete(ApiConstants.HOST + path + "/" + id);
+        return composeRequest().delete(ApiConstants.HOST + path + "/" + id);
     }
 
     public static Response sendInvalidGet(String path, String contentType) throws JSONException {
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", contentType);
-        return request.get(ApiConstants.HOST + path);
+        return composeRequest().contentType(contentType).get(ApiConstants.HOST + path);
     }
 
 
