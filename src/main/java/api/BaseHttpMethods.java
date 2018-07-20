b71c062d2baba689c2506e22af3144b6361cffeb
christinakornilova <30318622+fsage@users.noreply.github.com>
2018-07-20 20:35:14 +0300

    Api tests update method name.

--- src/main/java/api/BaseHttpMethods.java
+++ src/main/java/api/BaseHttpMethods.java
@@ -8,30 +8,30 @@ import utils.ApiConstants;
 
 public class BaseHttpMethods {
 
-    private static RequestSpecification composeRequest() {
+    private static RequestSpecification composeRequestSpec() {
         RequestSpecification request = RestAssured.given();
         request.header("Content-Type", "application/json");
         return request;
     }
 
     public static Response sendGet(String path) throws JSONException {
-        return composeRequest().get(ApiConstants.HOST + path);
+        return composeRequestSpec().get(ApiConstants.HOST + path);
     }
 
     public static Response sendPut(String path, int id, String jsonBody) {
-        return composeRequest().body(jsonBody).put(ApiConstants.HOST + path + "/" + id);
+        return composeRequestSpec().body(jsonBody).put(ApiConstants.HOST + path + "/" + id);
     }
 
     public static Response sendPost(String path, String jsonBody) {
-        return composeRequest().body(jsonBody).post(ApiConstants.HOST + path);
+        return composeRequestSpec().body(jsonBody).post(ApiConstants.HOST + path);
     }
 
     public static Response sendDelete(String path, int id) {
-        return composeRequest().delete(ApiConstants.HOST + path + "/" + id);
+        return composeRequestSpec().delete(ApiConstants.HOST + path + "/" + id);
     }
 
     public static Response sendInvalidGet(String path, String contentType) throws JSONException {
-        return composeRequest().contentType(contentType).get(ApiConstants.HOST + path);
+        return composeRequestSpec().contentType(contentType).get(ApiConstants.HOST + path);
     }
 
 
